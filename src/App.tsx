08878069f4fc6c5b38b25e816da63ae0e3bf27a4
ValRoss132 <valross1321@gmail.com>
2025-09-26 17:45:47 +0300

    feat: render cards with count limit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
+import MainPage from './pages/MainPage';
+import citiesData from './data/citiesData.json';
+
+const LIMIT = 8;
+
 function App() {
-  return <div></div>;
+  return <MainPage limit={LIMIT} data={citiesData} />;
 }
 
 export default App;

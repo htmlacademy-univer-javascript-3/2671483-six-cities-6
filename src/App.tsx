d6a10e744ee132f7d175ebb798de0afe56ac951f
ValRoss132 <valross1321@gmail.com>
2025-10-06 18:18:00 +0300

    feat: add notFound page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import MainPage from './pages/MainPage';
 
 import citiesData from './data/citiesData.json';
 import { LIMIT, AppRoute } from './const';
+import NotFoundPage from './pages/NotFoundPage';
+import LoginPage from './pages/LoginPage';
+import FavoritesPage from './pages/FavoritesPage';
+import OfferPage from './pages/OfferPage';
 
 function App() {
   return (
@@ -12,9 +16,10 @@ function App() {
           path={AppRoute.Root}
           element={<MainPage data={citiesData} limit={LIMIT} />}
         />
-        <Route path={AppRoute.Login} />
-        <Route path={AppRoute.Favorites} />
-        <Route path={AppRoute.Offer} />
+        <Route path={AppRoute.Login} element={<LoginPage />} />
+        <Route path={AppRoute.Favorites} element={<FavoritesPage />} />
+        <Route path={AppRoute.Offer} element={<OfferPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
   );

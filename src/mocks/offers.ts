3fb2edaa4b553df1332e9d37a7e858dc29eeb774
ValRoss132 <valross1321@gmail.com>
2025-10-07 19:13:56 +0300

    feat: add mocks data

---
+++ src/mocks/offers.ts
@@ -0,0 +1,95 @@
+import { City } from '../types/City';
+
+export const offers: City[] = [{
+  id: '14efc878-0d8a-4270-9667-bf5241632223',
+  title: 'The Joshua Tree House',
+  type: 'hotel',
+  price: 421,
+  previewImage: 'https://14.design.htmlacademy.pro/static/hotel/8.jpg',
+  city: {
+    name: 'Amsterdam',
+    location: {
+      latitude: 52.37454,
+      longitude: 4.897976,
+      zoom: 13
+    }
+  },
+  location: {
+    latitude: 52.36554,
+    longitude: 4.911976,
+    zoom: 16
+  },
+  isFavorite: false,
+  isPremium: false,
+  rating: 5
+},
+{
+  id: 'd08a53c2-33fc-4c09-8bb7-cdc6bef81a04',
+  title: 'The house among olive',
+  type: 'room',
+  price: 123,
+  previewImage: 'https://14.design.htmlacademy.pro/static/hotel/18.jpg',
+  city: {
+    name: 'Amsterdam',
+    location: {
+      latitude: 52.37454,
+      longitude: 4.897976,
+      zoom: 13
+    }
+  },
+  location: {
+    latitude: 52.385540000000006,
+    longitude: 4.902976,
+    zoom: 16
+  },
+  isFavorite: false,
+  isPremium: true,
+  rating: 4.2
+},
+{
+  id: '59f20fe1-4df0-404e-b9e5-9bf74a62f489',
+  title: 'Loft Studio in the Central Area',
+  type: 'hotel',
+  price: 337,
+  previewImage: 'https://14.design.htmlacademy.pro/static/hotel/13.jpg',
+  city: {
+    name: 'Amsterdam',
+    location: {
+      latitude: 52.37454,
+      longitude: 4.897976,
+      zoom: 13
+    }
+  },
+  location: {
+    latitude: 52.397540000000006,
+    longitude: 4.9099759999999995,
+    zoom: 16
+  },
+  isFavorite: false,
+  isPremium: false,
+  rating: 1.6
+},
+{
+  id: '7007da91-bc5c-4ffc-ba45-69e7deb300c0',
+  title: 'House in countryside',
+  type: 'room',
+  price: 280,
+  previewImage: 'https://14.design.htmlacademy.pro/static/hotel/12.jpg',
+  city: {
+    name: 'Amsterdam',
+    location: {
+      latitude: 52.37454,
+      longitude: 4.897976,
+      zoom: 13
+    }
+  },
+  location: {
+    latitude: 52.37454,
+    longitude: 4.881976,
+    zoom: 16
+  },
+  isFavorite: false,
+  isPremium: false,
+  rating: 4.6
+},
+];

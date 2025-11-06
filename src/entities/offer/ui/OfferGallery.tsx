7914df792c85834ad17de063479ed12d599bc81a
ValRoss132 <valross1321@gmail.com>
2025-11-06 18:45:08 +0300

    refactor(OfferPage): replace offer gallery with OfferGalerry component

---
+++ src/entities/offer/ui/OfferGallery.tsx
@@ -0,0 +1,17 @@
+type OfferGalleryProps = {
+  images: string[];
+};
+
+export function OfferGallery({ images }: OfferGalleryProps): JSX.Element {
+  return (
+    <div className="offer__gallery-container container">
+      <div className="offer__gallery">
+        {images.map((image) => (
+          <div key={image} className="offer__image-wrapper">
+            <img className="offer__image" src={image} alt="Photo studio" />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}

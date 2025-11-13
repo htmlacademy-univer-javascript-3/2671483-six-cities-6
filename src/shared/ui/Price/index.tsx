4d52bb7c0357edf727382b1641c358211608df1a
ValRoss132 <valross1321@gmail.com>
2025-11-13 19:25:49 +0300

    refactor(Price): add block prop for classnames

--- src/shared/ui/Price/index.tsx
+++ src/shared/ui/Price/index.tsx
@@ -2,13 +2,14 @@ import { ReactNode } from 'react';
 
 type PriceProps = {
   children: ReactNode;
+  block: 'place-card' | 'offer';
 };
 
-export function Price({ children }: PriceProps) {
+export function Price({ children, block }: PriceProps) {
   return (
-    <div className="place-card__price">
-      <b className="place-card__price-value">&euro;{children}</b>
-      <span className="place-card__price-text">&#47;&nbsp;night</span>
+    <div className={`${block}__price`}>
+      <b className={`${block}__price-value`}>&euro;{children}</b>
+      <span className={`${block}__price-text`}>&#47;&nbsp;night</span>
     </div>
   );
 }

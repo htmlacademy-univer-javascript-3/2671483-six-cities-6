a031366989d0bf0ddebd54c150d0f692a99b27cf
ValRoss132 <valross1321@gmail.com>
2025-12-08 15:43:55 +0300

    feat: configure redux thunk

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -1,9 +1,20 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { rootReducer } from './rootReducer';
 
-export const setupStore = () => configureStore({
-  reducer: rootReducer
-});
+import { createApi } from '../../shared/api/api';
+
+export const api = createApi();
+
+export const setupStore = () =>
+  configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        thunk: {
+          extraArgument: api,
+        },
+      }),
+  });
 
 export const store = setupStore();
 

b8fa0b0f4b2c62b944573a77fce17ce193d3b5bd
ValRoss132 <valross1321@gmail.com>
2025-12-10 19:11:27 +0300

    feat(offer): add thunks for fetching single and offers nearby

--- src/entities/offer/model/offers.thunks.ts
+++ src/entities/offer/model/offers.thunks.ts
@@ -2,7 +2,7 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import { AxiosInstance } from 'axios';
 import { ApiRoute } from '../../../shared/config/api-route';
 
-import type { Offers } from '../../../shared/types/Offer.type';
+import type { Offer, Offers } from '../../../shared/types/Offer.type';
 
 export const fetchOffersAction = createAsyncThunk<
   Offers,
@@ -12,5 +12,27 @@ export const fetchOffersAction = createAsyncThunk<
   }
 >('offersList/fetchOffers', async (_arg, { extra: api }) => {
   const { data } = await api.get<Offers>(ApiRoute.Offers);
+  return data;
+});
+
+export const fetchOfferAction = createAsyncThunk<
+  Offer,
+  string,
+  {
+    extra: AxiosInstance;
+  }
+>('offersList/fetchOffer', async (offerId, { extra: api }) => {
+  const { data } = await api.get<Offer>(ApiRoute.Offer(offerId));
+  return data;
+});
+
+export const fetchOffersNearbyAction = createAsyncThunk<
+  Offers,
+  string,
+  {
+    extra: AxiosInstance;
+  }
+>('offersList/fetchOffersNearby', async (offerId, { extra: api }) => {
+  const { data } = await api.get<Offers>(ApiRoute.OffersNearby(offerId));
   return data;
 });

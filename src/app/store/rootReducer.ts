ab74802713e98592547eab03b9f4645e57e46806
ValRoss132 <valross1321@gmail.com>
2025-12-12 01:52:18 +0300

    chore(app): integrate new reducers into rootReducer

--- src/app/store/rootReducer.ts
+++ src/app/store/rootReducer.ts
@@ -1,12 +1,16 @@
 import { combineReducers } from '@reduxjs/toolkit';
-import { citySelectorReducer } from '../../features/CitySelector/model/citySelectorSlice';
 import { currentOfferReducer } from '../../entities/offer/model/currentOfferSlice';
 import { offerListReducer } from '../../entities/offer/model/offersListSlice';
+import { userReducer } from '../../entities/user/model/userSlice';
+import { citySelectorReducer } from '../../features/CitySelector/model/citySelectorSlice';
+import { favoritesReducer } from '../../features/Favorites/model/favoritesSlice';
 import { sortOffersReducer } from '../../features/SortOffers/model/sortOffersSlice';
 
 export const rootReducer = combineReducers({
+  user: userReducer,
   city: citySelectorReducer,
   currentOffer: currentOfferReducer,
   offerList: offerListReducer,
-  sortOffers: sortOffersReducer
+  sortOffers: sortOffersReducer,
+  favoritesOffers: favoritesReducer,
 });

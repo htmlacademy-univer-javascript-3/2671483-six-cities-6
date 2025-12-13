9f4ebc251b1b3bdae580491fcaf2b58722ae2143
ValRoss132 <valross1321@gmail.com>
2025-12-13 05:18:25 +0300

    feat(error): implement error notification mechanism

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,6 +1,15 @@
-import axios, { AxiosInstance, InternalAxiosRequestConfig } from 'axios';
+import axios, {
+  AxiosError,
+  AxiosInstance,
+  InternalAxiosRequestConfig,
+} from 'axios';
+import { toast } from 'react-toastify';
 import { BASE_URL, REQUEST_TIMEOUT } from '../config/service';
+import { getErrorMessage } from '../lib/error-formatter';
 import { getToken } from '../lib/storage/token';
+import { shouldDisplayError } from './api-error-utils';
+
+import type { DetailErrorData } from '../types/Error.type';
 
 export const createApi = (): AxiosInstance => {
   const api = axios.create({
@@ -12,11 +21,22 @@ export const createApi = (): AxiosInstance => {
     const token = getToken();
 
     if (token) {
-      config.headers['x-token'] = token;
+      config.headers['X-Token'] = token;
     }
 
     return config;
   });
+
+  api.interceptors.response.use(
+    (response) => response,
+    (error: AxiosError<DetailErrorData>) => {
+      if (error.response && shouldDisplayError(error.response)) {
+        const errorMessage = getErrorMessage(error);
+        toast.error(errorMessage);
+      }
+      throw error;
+    }
+  );
 
   return api;
 };

45a6b1c613b24d66e401d5f4605a1476cd2c6790
ValRoss132 <valross1321@gmail.com>
2025-12-15 19:49:38 +0300

    refactor: implement model layer for OfferDetails feature

---
+++ src/features/OfferContent/model/offerDetailsSlice.ts
@@ -0,0 +1,54 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { fetchOfferDataAction } from './offer-details.thunks';
+
+import type { FullOffer, Offers } from '../../../shared/types/Offer.type';
+import { IReview } from '../../../shared/types/Review.type';
+
+interface currentOfferState {
+  offer: FullOffer | null;
+  offersNearby: Offers;
+  reviews: IReview[];
+  isLoading: boolean;
+  hasError: boolean;
+}
+
+const INITIAL_STATE: currentOfferState = {
+  offer: null,
+  offersNearby: [],
+  reviews: [],
+  isLoading: false,
+  hasError: false,
+};
+
+export const offerDetailsSlice = createSlice({
+  name: 'currentOffer',
+  initialState: INITIAL_STATE,
+  reducers: {
+    clearOfferDetails: (state) => {
+      state.offer = null;
+      state.offersNearby = [];
+      state.reviews = [];
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchOfferDataAction.pending, (state) => {
+        state.isLoading = true;
+        state.hasError = false;
+      })
+      .addCase(fetchOfferDataAction.fulfilled, (state, action) => {
+        state.offer = action.payload.offer;
+        state.offersNearby = action.payload.offersNearby;
+        state.reviews = action.payload.reviews;
+        state.isLoading = false;
+      })
+      .addCase(fetchOfferDataAction.rejected, (state) => {
+        state.isLoading = false;
+        state.hasError = true;
+      });
+  },
+});
+
+export const { clearOfferDetails } = offerDetailsSlice.actions;
+
+export const offerDetailsReducer = offerDetailsSlice.reducer;

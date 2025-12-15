0bb3d81ea3f1ac3820b3aa8045a7ea280e61c163
ValRoss132 <valross1321@gmail.com>
2025-12-15 19:53:37 +0300

    feat(store): add offerDetailsReducer into rootReducer

--- src/app/store/rootReducer.ts
+++ src/app/store/rootReducer.ts
@@ -1,15 +1,15 @@
 import { combineReducers } from '@reduxjs/toolkit';
-import { currentOfferReducer } from '../../entities/offer/model/currentOfferSlice';
 import { offerListReducer } from '../../entities/offer/model/offersListSlice';
 import { userReducer } from '../../entities/user/model/userSlice';
 import { citySelectorReducer } from '../../features/CitySelector/model/citySelectorSlice';
 import { favoritesReducer } from '../../features/Favorites/model/favoritesSlice';
+import { offerDetailsReducer } from '../../features/OfferContent/model/offerDetailsSlice';
 import { sortOffersReducer } from '../../features/SortOffers/model/sortOffersSlice';
 
 export const rootReducer = combineReducers({
   user: userReducer,
   city: citySelectorReducer,
-  currentOffer: currentOfferReducer,
+  offer: offerDetailsReducer,
   offerList: offerListReducer,
   sortOffers: sortOffersReducer,
   favoritesOffers: favoritesReducer,

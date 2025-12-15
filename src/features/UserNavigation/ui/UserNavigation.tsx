b7a01355fb88b22237451ceb02d9e57bce5fc4cf
ValRoss132 <valross1321@gmail.com>
2025-12-15 14:29:03 +0300

    refactor(nav): replace <a> tag with <Link>

--- src/features/UserNavigation/ui/UserNavigation.tsx
+++ src/features/UserNavigation/ui/UserNavigation.tsx
@@ -23,13 +23,16 @@ function UserNavigation() {
       <ul className="header__nav-list">
         {isAuth && (
           <li className="header__nav-item user">
-            <a className="header__nav-link header__nav-link--profile" href="#">
+            <Link
+              className="header__nav-link header__nav-link--profile"
+              to={AppRoute.Favorites}
+            >
               <div className="header__avatar-wrapper user__avatar-wrapper"></div>
               <span className="header__user-name user__name">
                 {user?.email}
               </span>
               <span className="header__favorite-count">3</span>
-            </a>
+            </Link>
           </li>
         )}
         <li className="header__nav-item">

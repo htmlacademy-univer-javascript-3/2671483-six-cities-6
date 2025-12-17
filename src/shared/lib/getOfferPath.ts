2854aeaffcca5463b7efb6b5fd857d1a50073c91
ValRoss132 <valross1321@gmail.com>
2025-12-17 18:18:52 +0300

    refactor(shared): rename getOfferPath to utils and add helper functions

--- src/shared/lib/getOfferPath.ts
+++
@@ -1,3 +0,0 @@
-import { AppRoute } from '../config/route';
-
-export const getOfferPath = (id: string | number) => AppRoute.Offer.replace(':offerId', String(id));
---
+++ src/shared/lib/utils.ts
@@ -0,0 +1,10 @@
+import { AppRoute } from '../config/route';
+import { Offers } from '../types/Offer.type';
+
+export const getOfferPath = (id: string | number) =>
+  AppRoute.Offer.replace(':offerId', String(id));
+
+export const getLimitedPoints = (limit: number, points: Offers) => {
+  const safeLimit = Math.max(0, limit);
+  return points.slice(0, safeLimit);
+};

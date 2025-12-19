fc837d3a24d39117e0a9ce95f4c97097b1b98234
ValRoss132 <valross1321@gmail.com>
2025-12-19 15:48:11 +0300

    perf(sort-offers): stabilize handlers and prevent redundant sorting

--- src/features/SortOffers/model/useSortOffers.ts
+++ src/features/SortOffers/model/useSortOffers.ts
@@ -1,27 +1,35 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { changeSortOption } from './sortOffersSlice';
 import { setOffersList } from '../../../entities/offer/model/offersListSlice';
 import { useAppSelector } from '../../../shared/lib/hooks/redux';
 import { getSortedOffers } from './sort-utils';
+import { changeSortOption } from './sortOffersSlice';
 
 import { SORT_OPTIONS } from '../../../shared/config/const';
 
 export function useSortOffers() {
   const dispatch = useDispatch();
   const activeSort = useAppSelector((state) => state.sortOffers.sortOption);
-  const offers = useAppSelector((state) => state.offerList.list);
+  const offers = useAppSelector((state) => state.offerList.filteredList);
 
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
-  const handleToggle = () => {
+  const handleToggle = useCallback(() => {
     setIsOpen((prev) => !prev);
-  };
+  }, []);
 
-  const handleSelect = (option: (typeof SORT_OPTIONS)[number]) => {
-    dispatch(changeSortOption(option));
-    dispatch(setOffersList(getSortedOffers(offers, option)));
-    setIsOpen(false);
-  };
-  return {isOpen, activeSort, handleToggle, handleSelect};
+  const handleSelect = useCallback(
+    (option: (typeof SORT_OPTIONS)[number]) => {
+      if (option === activeSort) {
+        setIsOpen(false);
+        return;
+      }
+
+      dispatch(changeSortOption(option));
+      dispatch(setOffersList(getSortedOffers(offers, option)));
+      setIsOpen(false);
+    },
+    [dispatch, offers, activeSort]
+  );
+  return { isOpen, activeSort, handleToggle, handleSelect };
 }

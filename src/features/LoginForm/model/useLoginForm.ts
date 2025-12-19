2a822e1f95a3cd1f0a13126b99b50c167d9764a1
ValRoss132 <valross1321@gmail.com>
2025-12-19 15:58:55 +0300

    perf(login-form): optimize input rendering using memo and useCallback

--- src/features/LoginForm/model/useLoginForm.ts
+++ src/features/LoginForm/model/useLoginForm.ts
@@ -1,4 +1,4 @@
-import { ChangeEvent, FormEvent, useState } from 'react';
+import { ChangeEvent, FormEvent, useCallback, useState } from 'react';
 import { useAppDispatch } from '../../../shared/lib/hooks/redux';
 
 import { loginAction } from '../../../entities/user/model/user.thunks';
@@ -11,21 +11,24 @@ export const useLoginForm = () => {
     password: '',
   });
 
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+  const handleChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
     setFormData((prev) => ({
       ...prev,
       [name]: value,
     }));
-  };
+  }, []);
 
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+  const handleSubmit = useCallback(
+    (event: FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
 
-    if (formData.email && formData.password) {
-      dispatch(loginAction(formData));
-    }
-  };
+      if (formData.email && formData.password) {
+        dispatch(loginAction(formData));
+      }
+    },
+    [dispatch, formData]
+  );
 
   return { formData, handleChange, handleSubmit };
 };

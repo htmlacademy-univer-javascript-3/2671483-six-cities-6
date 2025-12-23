d49d36a0632e261c9d51732e9d1a5a3a86c98b57
ValRoss132 <valross1321@gmail.com>
2025-12-23 15:15:30 +0300

    feat(offer): add selectors

---
+++ src/widgets/offer-content/model/offer-details.selectors.ts
@@ -0,0 +1,24 @@
+import { createSelector } from '@reduxjs/toolkit';
+import { RootState } from '../../../app/store';
+
+export const selectOfferDetails = (state: RootState) => state.offer;
+
+export const selectOffer = (state: RootState) => state.offer.offer;
+
+export const selectOffersNearby = (state: RootState) =>
+  state.offer.offersNearby;
+
+export const selectReviews = (state: RootState) => state.offer.reviews;
+
+export const selectIsOfferLoading = (state: RootState) => state.offer.isLoading;
+export const selectOfferHasError = (state: RootState) => state.offer.hasError;
+
+export const selectSortedReviews = createSelector([selectReviews], (reviews) =>
+  [...reviews]
+    .sort((a, b) => {
+      const dateA = new Date(a.date).getTime();
+      const dateB = new Date(b.date).getTime();
+      return dateB - dateA;
+    })
+    .slice(0, 10)
+);

11daf59aacd6e52fdc5fa110a5fd6a42cd65c2d9
ValRoss132 <valross1321@gmail.com>
2025-12-23 16:36:55 +0300

    refactor(offer-page): extract Host component and use it in OfferDetails

--- src/widgets/offer-content/ui/offer-details.tsx
+++ src/widgets/offer-content/ui/offer-details.tsx
@@ -12,6 +12,7 @@ import { Mark, Price, RatingStars } from '../../../shared/ui';
 
 import type { FullOffer } from '../../../shared/types/offer.type';
 import type { IReview } from '../../../shared/types/review.type';
+import { Host } from '../../../shared/ui/host';
 
 type OfferDetailsProps = {
   offer: FullOffer;
@@ -62,29 +63,7 @@ function OfferDetails({ offer, reviews }: OfferDetailsProps): JSX.Element {
             <li className="offer__inside-item">Fridge</li>
           </ul>
         </div>
-        {/* === HOST === */}
-        <div className="offer__host">
-          <h2 className="offer__host-title">Meet the host</h2>
-          <div className="offer__host-user user">
-            <div className="offer__avatar-wrapper offer__avatar-wrapper--pro user__avatar-wrapper">
-              <img
-                className="offer__avatar user__avatar"
-                src={offer.host.avatarUrl}
-                width="74"
-                height="74"
-                alt="Host avatar"
-              />
-            </div>
-            <span className="offer__user-name">{offer.host.name}</span>
-            {offer.host.isPro && (
-              <span className="offer__user-status">Pro</span>
-            )}
-          </div>
-          <div className="offer__description">
-            <p className="offer__text">{offer.description}</p>
-          </div>
-        </div>
-        {/* === {} === */}
+        <Host offer={offer} />
         <section className="offer__reviews reviews">
           <ReviewList reviews={reviews} reviewCount={reviewCount} />
           {isAuth && <ReviewForm />}
